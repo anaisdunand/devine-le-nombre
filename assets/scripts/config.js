0a7bbe34c3733ea70bf175134e9aa16f218908b2
Anaïs Dunand <anais.dunand@orange.fr>
2025-07-21 18:23:05 +0200

    Import du JS

---
+++ assets/scripts/config.js
@@ -0,0 +1,8 @@
+let secretNumber
+let attemptsNumber
+
+const input = document.querySelector(".box__input")
+const textZone = document.querySelector(".box__text")
+
+const checkButton = document.getElementById("check")
+const replayButton = document.getElementById("replay")
---
+++ assets/scripts/script.js
@@ -0,0 +1,52 @@
+function startGame() {
+    secretNumber = Math.floor(Math.random() * 1000) + 1
+    attemptsNumber = 0
+}
+
+function resetGame() {
+    startGame()
+
+    input.value = ""
+    input.placeholder = "Ex: 420"
+    input.classList.remove("box__input--error")
+    textZone.innerHTML = "Devine à quel nombre je pense &#x1F609;"
+    textZone.classList.remove("box__text--won", "box__text--clue")
+    checkButton.disabled = false
+}
+
+input.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        checkButton.click()
+    }
+})
+
+checkButton.addEventListener("click", () => {
+    const userNumber = Number(input.value)
+
+    if (userNumber < 1 || userNumber > 1000 || userNumber === "") {
+        input.classList.add("box__input--error")
+        textZone.textContent = "On a dit un nombre entre 1 et 1000..."
+    } else {
+        input.classList.remove("box__input--error")
+        attemptsNumber++
+
+        if (userNumber === secretNumber) {
+            textZone.classList.add("box__text--won")
+            textZone.innerHTML = `Bravo !<br>Tu as trouvé en<br>${attemptsNumber} tentatives`
+            checkButton.disabled = true
+        } else if (userNumber < secretNumber) {
+            textZone.classList.add("box__text--clue")
+            textZone.textContent = "C'est plus !"
+        } else {
+            textZone.classList.add("box__text--clue")
+            textZone.textContent = "C'est moins !"
+        }
+    }
+
+    input.placeholder = `${input.value}`
+    input.value = ""
+})
+
+replayButton.addEventListener("click", () => {
+    resetGame()
+})
